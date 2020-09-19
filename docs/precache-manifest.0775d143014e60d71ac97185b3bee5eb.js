4af927c8b9192e8880912ed41d908e0990d6b949
Luis Hiram Díaz <luishiram.ds@gmail.com>
2020-09-19 13:31:29 -0500

    docs agregado y actualizado

---
+++ docs/precache-manifest.0775d143014e60d71ac97185b3bee5eb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7f82f55fabc1d145790e3824a2f43aea",
+    "url": "/index.html"
+  },
+  {
+    "revision": "72380b494d3e5fa954cd",
+    "url": "/static/js/2.7168b68b.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.7168b68b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "720425e086d3e99b6f0e",
+    "url": "/static/js/main.3f25dad2.chunk.js"
+  },
+  {
+    "revision": "94d7ca25d4df19621824",
+    "url": "/static/js/runtime-main.b7f813af.js"
+  }
+]);
