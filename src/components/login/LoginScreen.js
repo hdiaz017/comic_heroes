5c330a2dd6f655814cd0edc18bed5a9ab25e3300
Luis Hiram Díaz <luishiram.ds@gmail.com>
2020-09-19 13:12:11 -0500

    rutas publicas y privadas. recordatorio de ultima ruta

--- src/components/login/LoginScreen.js
+++ src/components/login/LoginScreen.js
@@ -1,8 +1,19 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { AuthContext } from '../../auth/AuthContext';
+import { types } from '../../types/types';
 
 export const LoginScreen = ({ history }) => {
+   const { dispatch } = useContext(AuthContext);
+
    const handleLogin = () => {
-      history.replace('/');
+      const lastPath = localStorage.getItem('lastPath') || '/';
+      console.log(lastPath);
+      const action = {
+         payload: { name: 'hdiaz17' },
+         type: types.login,
+      };
+      dispatch(action);
+      history.replace(lastPath);
    };
    return (
       <div className='container mt-5'>
--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -1,17 +1,30 @@
-import React from 'react';
+import React, { useContext } from 'react';
 
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { AuthContext } from '../auth/AuthContext';
 
 import { LoginScreen } from '../components/login/LoginScreen';
 import { DashBoardRouter } from './DashBoardRouter';
+import { PrivateRoute } from './PrivateRoute';
+import { PublicRoute } from './PublicRoute';
 
 export const AppRouter = () => {
+   const { user } = useContext(AuthContext);
    return (
       <Router>
          <div>
             <Switch>
-               <Route exact path='/login' component={LoginScreen} />
-               <Route path='/' component={DashBoardRouter} />
+               <PublicRoute
+                  exact
+                  path='/login'
+                  component={LoginScreen}
+                  isAuthenticated={user.logged}
+               />
+               <PrivateRoute
+                  isAuthenticated={user.logged}
+                  path='/'
+                  component={DashBoardRouter}
+               />
             </Switch>
          </div>
       </Router>
